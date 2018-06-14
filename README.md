bc952c9e2981aa0d70990a4060be5201e635bec3
Chunlin Zhang <zhangchunlin@gmail.com>
2018-06-14 17:26:24 +0800

    step12: add product config

---
+++ README.md
@@ -0,0 +1,15 @@
+来源:
+- [Vue.js 实战之工程篇](https://segmentfault.com/l/1500000009448189),使用和此教程一致的范例
+- 每一步一个提交,可以看git历史来学习和本地验证测试
+
+内容:
+- 从无到有建立一个webpack项目
+- 安装 webpack 及相关的一系列 npm 模块和工具
+- 使用 webpack-dev-server 作为调试服务器
+- 最简单的 webpack 项目
+- webpack build
+- 编译css
+- 使用 ExtractTextPlugin 合并 css
+- vue单文件组件的支持
+- 对于图片文件的编译支持
+- 生产环境方式的build
---
+++ webpack.prod.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.config')
+
+webpackBaseConfig.plugins = [];
+
+module.exports = merge(webpackBaseConfig, {
+    output: {
+        publicPath: '/dist/',
+        filename: '[name].[hash].js'
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].[hash].css',
+            allChunks: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: '../index_prod.html',
+            template: './index.ejs',
+            inject: false
+        })
+    ]
+})
